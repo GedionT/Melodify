7370266fcba669b1f944dc565625e761bcc0ab58
Yusra Irfan <yusrairfan99@hotmail.com>
2020-06-13 02:47:40 -0400

    sound works with front end angular

--- Melodify/src/app/home/home.component.ts
+++ Melodify/src/app/home/home.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {MusicServiceService} from '../music-service.service';
-
+import { MusicServiceService } from '../music-service.service';
+import playKeyboard from '../helpers/playKeyboard';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private musicService: MusicServiceService) {}
 
-  constructor(private musicService : MusicServiceService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  play(url){
+  play(url) {
     console.log(url);
-    this.musicService.scrapeSite(url).subscribe(data =>{
+    this.musicService.scrapeSite(url).subscribe((data) => {
       console.log(data);
-    })
+      playKeyboard(data);
+    });
   }
-
-
-  
-
 }
