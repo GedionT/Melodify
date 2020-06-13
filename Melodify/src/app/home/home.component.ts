a59741cc283da8b8e7672201f39ea3a669b839b1
Yusra Irfan <yusrairfan99@hotmail.com>
2020-06-13 19:00:44 -0400

    stopping part way

--- Melodify/src/app/home/home.component.ts
+++ Melodify/src/app/home/home.component.ts
@@ -1,26 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import {MusicServiceService} from '../music-service.service';
+import { MusicServiceService } from '../music-service.service';
+import { playKeyboard, toKeyboardArray } from '../helpers/playKeyboard';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent implements OnInit {
+  constructor(private musicService: MusicServiceService) {}
+  stop = { value: false };
+  text = [];
+  notesArray = [];
+  index = 0;
+  elem: Element;
+  renderFlag = false;
 
-  constructor(private musicService : MusicServiceService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  play(url) {
+    this.stop.value = false;
+    console.log(this.stop);
+    this.musicService.scrapeSite(url).subscribe((arrayTextLength) => {
+      let i = 0;
+      arrayTextLength = arrayTextLength.map((a) => [a[0], a[1], i++]);
+      // console.log(arrayTextLength);
+      this.text = arrayTextLength;
+      this.notesArray = toKeyboardArray(arrayTextLength);
+      // console.log('notes array: ' + this.notesArray);
+      playKeyboard(this.notesArray, this.stop);
+    });
   }
 
-  play(url){
-    console.log(url);
-    this.musicService.scrapeSite(url).subscribe(data =>{
-      console.log(data);
-    })
+  timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
+  stopButton() {
+    this.stop.value = true;
+    this.index = 0;
+    this.renderFlag = false;
+    //remove all the highlight here 
+    this.text=[];
+    this.notesArray=[];
+  }
 
-  
+  async setRenderTrue() {
+    this.renderFlag = true;
 
+    for (let i of this.text) {
+      if (this.index <= this.text.length) {
+        // if (document.getElementsByClassName('highlight')[0]!== undefined){
+        // document.getElementsByClassName('highlight')[0].classList.remove('highlight')
+        document.getElementById('text' + this.index.toString()).classList.add('highlight');
+        document.getElementById('note' + this.index.toString()).classList.add('highlight');
+        console.log('dfdfd' + this.stop.value);
+
+        await this.timeout(i[1] * 200);
+        if (this.stop.value) break;
+        this.index++;
+      }
+    }
+    this.index = 0;
+    return;
+  }
 }
+
