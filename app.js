4a9003f4a7f87785f1b5defae6cccd8d86112b33
gediont <teshomegedion@gmail.com>
2020-06-14 12:47:36 +0300

    minor change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('x-powered-by', false);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('./melodify/dist/Melodify'));
+app.use(express.static(path.join(__dirname, '/melodify/dist/Melodify')));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect - #localhost will be replaced with domain name for prod server
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 })
 
 app.get('/*', function(req, res, next) {
-    res.sendFile('./melodify/dist/Melodify/index.html');
+    res.sendFile(path.resolve(__dirname + '/melodify/dist/Melodify/index.html'));
 });
 
 app.post('/scrape', function(req, res, next) {
