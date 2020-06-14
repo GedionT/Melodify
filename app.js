4d938049a822a98589e36cf5e4fa337075d8e459
gediont <teshomegedion@gmail.com>
2020-06-14 13:12:08 +0300

    pleasse work

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('x-powered-by', false);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/melodify/dist/Melodify')));
+app.use(express.static(path.join(__dirname, 'app/melodify/dist/Melodify')));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect - #localhost will be replaced with domain name for prod server
