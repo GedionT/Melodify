219f5a76e175a27facbb060f4ebf4324cf26ebb2
gedion_t <Teshomegedion@gmail.com>
2020-06-18 07:54:16 +0300

    Detached Headless Run.
    Final Fix followup

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('*', function(req, res, next) {
 const pAll = '';
 
 const scrape = async (url) => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-setuid-sandbox'], headless: false });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-setuid-sandbox'], headless: true });
   const page = await browser.newPage();
   await page.goto(url);
 
